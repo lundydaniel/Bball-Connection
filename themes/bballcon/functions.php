f6dd2f6a2bef0830f69b76d457e5751ec491ec88
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-02-13 14:15:19 -0500

    Finished the initial set up for Bball Connection custom theme.

--- themes/bballcon/functions.php
+++ themes/bballcon/functions.php
@@ -7,9 +7,9 @@
  * @package Bball_Connection
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( 'bballcon_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( 'bballcon_VERSION', '1.0.0' );
 }
 
 /**
@@ -70,18 +70,6 @@ function bballcon_setup() {
 		)
 	);
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'bballcon_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
 
@@ -138,21 +126,13 @@ add_action( 'widgets_init', 'bballcon_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bballcon_scripts() {
-	wp_enqueue_style( 'bballcon-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'bballcon-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'bballcon-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_style( 'bballcon-style', get_stylesheet_uri(), array(), bballcon_VERSION );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'bballcon_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -168,11 +148,3 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
