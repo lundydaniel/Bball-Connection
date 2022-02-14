005379a8f4cc74b9846e0378f9378435ba4dc3cb
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-02-13 21:13:56 -0500

    Deleted wordpress information/links from my footer. Added some theme.json styles

--- themes/bballcon/footer.php
+++ themes/bballcon/footer.php
@@ -13,13 +13,6 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bballcon' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bballcon' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bballcon' ), 'bballcon', '<a href="https://daniellundydev.ca">Daniel Lundy</a>' );
