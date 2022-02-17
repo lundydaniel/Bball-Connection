8759ed7915598a33e06c189398b304f1f6f39215
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-02-17 14:52:06 -0500

    Removed the site link displayed under the logo. No need.

--- themes/bballcon/header.php
+++ themes/bballcon/header.php
@@ -33,12 +33,8 @@
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
 			endif;
-			$bballcon_description = get_bloginfo( 'description', 'display' );
+			$bballcon_description = get_bloginfo( 'description', 'display', );
 			if ( $bballcon_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $bballcon_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
