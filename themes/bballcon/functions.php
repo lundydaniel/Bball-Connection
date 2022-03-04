aa82d9cbaf1822755ba3031e28f7fe2a22ce2fc3
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-03-04 10:27:03 -0500

    Changed menu id from 1 to primary in header.php and functions.php.

--- themes/bballcon/functions.php
+++ themes/bballcon/functions.php
@@ -49,7 +49,7 @@ function bballcon_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'bballcon' ),
+			'menu-primary' => esc_html__( 'Primary', 'bballcon' ),
 		)
 	);
 
