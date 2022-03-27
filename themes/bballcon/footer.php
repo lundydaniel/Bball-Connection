71cc4bc6afd4616e5135ba0ab85d3029e7704a5d
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-03-26 21:52:32 -0400

    Added social media menu dynamically to footer.

--- themes/bballcon/footer.php
+++ themes/bballcon/footer.php
@@ -26,11 +26,24 @@
 			?>
 		</div>
 	</div>
+	<!--Custom footer content -->
+
 		<div class="site-info bottomFooter">
+			<!-- Adding the copyrite info -->
+			<div>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( '&copy; %1$s by %2$s.', 'bballcon' ), 'Bball Connection', '<a href="https://daniellundydev.ca">Daniel Lundy</a>' );
 				?>
+			</div>	
+			<!-- Adding Social Media Menu -->
+			<div>
+				<ul>
+					<li><a href="<?php echo esc_url( get_theme_mod( 'bballcon_facebook_url' ) ); ?>"><?php echo get_theme_mod( 'bballcon_facebook_title' ); ?></a></li>
+					<li><a href="<?php echo esc_url( get_theme_mod( 'bballcon_instagram_url' ) ); ?>"><?php echo get_theme_mod( 'bballcon_instagram_title' ); ?></a></li>
+					<li><a href="<?php echo esc_url( get_theme_mod( 'bballcon_twitter_url' ) ); ?>"><?php echo get_theme_mod( 'bballcon_twitter_title' ); ?></a></li>
+				</ul>
+			<div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
