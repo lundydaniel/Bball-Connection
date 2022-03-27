06c6fd785453295dd4f57e8f13a8c012ab8ddd1e
Daniel Lundy <daniel.lundy@dcmail.ca>
2022-03-27 16:06:58 -0400

    Changed the name of the file.

--- themes/bballcon/templates/bballcon-sidebar.php
+++ themes/bballcon/templates/bballcon-sidebar.php
@@ -19,13 +19,9 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
+			
 			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			echo 'testing this is working'
 
 		endwhile; // End of the loop.
 		?>
